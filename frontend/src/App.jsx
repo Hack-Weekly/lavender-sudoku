39d6ef79560305555641e70350359257ab86bdf6
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-08-13 11:04:14 +0100

    Added basic frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className="text-center text-4xl text-blue-400">sudoku</p>
+    </div>
+  );
+}
+
+export default App;
