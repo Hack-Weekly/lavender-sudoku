5429d7adc098b7367fa67cca26de408dbb31385b
Ian Stewart <ianstewart4@outlook.com>
2023-08-14 17:27:54 -0400

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { LandingPage } from './pages/LandingPage';
+import { FormPage } from './pages/FormPage';
 
 function App() {
   return (
    <>
    <Routes>
     <Route path='/' element={<LandingPage />} />
+    <Route path="/login" element={<FormPage isLogin={true} />} />
+    <Route path="/signup" element={<FormPage isLogin={false} />} />
    </Routes>
    </>
   );
