a663e388af5e7edf60d2e6140812add2bc0ecd77
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-08-15 11:55:54 +0100

    fixed conflit 2.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { LandingPage } from './pages/LandingPage';
-import { Login } from './pages/Login';
-import { Signup } from './pages/Signup';
 
 function App() {
   return (
