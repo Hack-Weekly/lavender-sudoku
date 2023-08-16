998946792f808a6045a23e6ef0c5c0d690aca6c2
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-08-16 17:42:26 +0100

    Added missing files

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -10,34 +10,59 @@ export const Signup = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [isInvalid, setIsInvalid] = useState(false);
   const navigate = useNavigate();
-  const handleSignUp = async() => {
-    try{
-      await register({username : username, email : email, password : password, password2 : password})
+  const handleSignUp = async () => {
+    try {
+      await register({
+        username: username,
+        email: email,
+        password: password,
+        password2: password,
+      });
       navigate("/");
-    }catch(error){
+    } catch (error) {
       setIsInvalid(true);
-      const errorData = error.response.data;
-      setErrorMessage("Please enter a valid :")
-      for(const key in errorData){
-        if(key !== "password2"){
-        setErrorMessage((prev)=> prev + " " + key);
+      if (error.response && error.response.data) {
+        const errorData = error.response.data;
+        setErrorMessage("Please enter a valid :");
+        for (const key in errorData) {
+          if (key !== "password2") {
+            setErrorMessage((prev) => prev + " " + key);
+          }
         }
       }
     }
-  }
+  };
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-b from-blue-300 to-blue-500">
       <div className="h-4/5 w-2/3 bg-white rounded-lg flex flex-col items-center p-8 shadow-lg gap-4">
         <h1 className="text-4xl font-semibold text-purple-600 mb-4">Sign up</h1>
 
-        <div className="w-full max-w-md flex flex-col gap-4" onChange={()=>{setErrorMessage("");setIsInvalid(false)}}>
-          <InputForm placeholder="Username" setField={setUsername} isInvalid={isInvalid} />
+        <div
+          className="w-full max-w-md flex flex-col gap-4"
+          onChange={() => {
+            setErrorMessage("");
+            setIsInvalid(false);
+          }}
+        >
+          <InputForm
+            placeholder="Username"
+            setField={setUsername}
+            isInvalid={isInvalid}
+          />
           <InputForm placeholder="email" setField={setEmail} isInvalid={isInvalid} />
-          <InputForm placeholder="Password" type="password" setField={setPassword} isInvalid={isInvalid} />
-          <button onClick={handleSignUp} className="bg-blue-600 text-white px-8 py-3 rounded-md text-lg font-semibold hover:bg-blue-700 transition duration-300 ease-in-out">
+          <InputForm
+            placeholder="Password"
+            type="password"
+            setField={setPassword}
+            isInvalid={isInvalid}
+          />
+          <button
+            onClick={handleSignUp}
+            className="bg-blue-600 text-white px-8 py-3 rounded-md text-lg font-semibold hover:bg-blue-700 transition duration-300 ease-in-out"
+          >
             Sign up
           </button>
-              <div  className="text-red-400 ">{errorMessage}</div>
+          <div className="text-red-400 ">{errorMessage}</div>
         </div>
         <p>
           You already have an account?{" "}
