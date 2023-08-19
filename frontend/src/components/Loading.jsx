2d09126d0e11bacd160c7419c8bd1ee40ef401d8
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-08-19 16:53:41 +0100

    Changed loading styling

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -2,8 +2,8 @@ import { Spinner } from "@material-tailwind/react";
 
 export const Loading = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-b from-blue-300 to-blue-500">
-      <div className="h-4/5 w-2/3 bg-white rounded-lg flex flex-col items-center justify-center p-8 shadow-lg gap-3">
+    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gradient-to-b from-white-600 to-white-500">
+      <div className="h-6/7 w-2/3 bg-gray-50 rounded-2xl border-2 border-gray-400 flex flex-col items-center p-8 shadow-2xl gap-3">
         <Spinner />
       </div>
     </div>
