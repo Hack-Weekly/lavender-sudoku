826804411e417fdc5bd11c01e22d2ee80aad2914
Ahmed Klabi <ahmetklabi10@gmail.com>
2023-08-20 16:11:18 +0100

    Changed baseurl

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://lavendersnake.alexdeathway.me",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
